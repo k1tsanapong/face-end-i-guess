b84b3860e247143498ce992b2693a21ca10bf2e1
KITTINARIN NAJAI <6431503004@lamduan.mfu.ac.th>
2022-11-15 23:28:00 +0700

    update table

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,21 +1,107 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-simple-table
+    fixed-header
+    height="620px"
+  >
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
-export default {
-  name: 'InspirePage'
-}
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },{
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+
+        ],
+      }
+    },
+  }
 </script>
