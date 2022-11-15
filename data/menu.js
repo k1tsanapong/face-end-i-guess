e8074a5f2de1858f0dec9704556b34851466cb1f
KITTINARIN NAJAI <6431503004@lamduan.mfu.ac.th>
2022-11-15 20:43:04 +0700

    init

---
+++ data/menu.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+      icon: "mdi-apps,",
+      
+      title: "Logo",
+      to: "/",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "รายการโปรด",
+      to: "/inspire",
+    },
+    {
+        icon: "mdi-moon-waxing-crescent",
+        title: "หน้าหลัก",
+        to: "/6431503002"
+    },
+    {
+        icon: "mdi-apple-icloud",
+        title: "ราการเพิ่มเติม",
+        to: "/6431503026"
+    },
+    {
+        icon: "mdi-sun-compass",
+        title: "ดาวน์โหลด",
+        to: "/6431503042"
+    },
+    {
+        icon: "mdi-apple-icloud",
+        title: "ปักหมุด",
+        to: "/6431503026"
+    },
+    {
+        icon: "mdi-apple-icloud",
+        title: "เวอร์ชั่น",
+        to: "/6431503026"
+    },
+    {
+        icon: "mdi-apple-icloud",
+        title: "ออกจากระบบ",
+        to: "/6431503026"
+    },
+  ]
+  export default data;
