239400e8ef76155cb09a552aad6d676ae98efa27
KITTINARIN NAJAI <6431503004@lamduan.mfu.ac.th>
2022-11-17 20:34:14 +0700

    increase search, calendar, privilege, status

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,110 @@
 <template>
 <v-data-table v-model="selected" :headers="headers" :items="desserts" :single-select="singleSelect" item-key="name" show-select class="elevation-1">
-  <template v-slot:item.name="{ item }">
+  
+    <template v-slot:top>
+        <v-row>
+            <v-col
+            class="d-flex"
+        cols="1"
+        sm="4"
+        md="4"
+        >
+            <v-text-field
+          v-model="search"
+          label="Search"
+          prepend-icon="mdi-magnify"
+          class="mx-4"
+        ></v-text-field>
+        </v-col>
+
+        <v-col
+        class="d-flex"
+        
+      cols="12"
+      sm="3"
+      md="3"
+    >
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="calendar"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="date"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+
+
+
+        </v-col>
+
+        <v-col
+        class="d-flex"
+        cols="12"
+        sm="2"
+        md="2"
+
+
+        >
+            <v-select
+          :items="privilege"
+          label="privilege"
+          
+        ></v-select>
+        </v-col>
+
+        <v-col
+        class="d-flex"
+        cols="12"
+        sm="2"
+        md="2"
+
+
+        >
+            <v-select
+          :items="status"
+          label="status"
+          
+        ></v-select>
+        </v-col>
+
+
+        </v-row>
+      </template>
+  
+    <template v-slot:item.name="{ item }">
     <h4 @click="ontest(item)">{{item.name}}</h4>
   </template>
   <template v-slot:footer.page-text>
@@ -16,7 +120,10 @@ export default {
     data() {
         return {
             singleSelect: false,
-            selected: [],
+            privileged: ['บุคคลในครอบครัวของพนักงานของมหาวิทยาลัยแม่ฟ้าหลวง', 'ตรวจสุขภาพ (ชำระเงินเอง รัฐวิสาหกิจ)', 'ตรวจสุขภาพ (ชำระเงินเอง ราชการ)', 'บุคคลในครอบครัวพนักงาน มฟล.','ตรวจสุขภาพหน่วยงานเอกชน','ผู้ป่วยในความอนุเคราะห์','มหาวิทยาลัยแม่ฟ้าหลวง','โครงการทันตกรรม','พนักงาน รพ.มฟล.','ผู้ป่วยบุคคลพิเศษ','รัฐวิสาหกิจี','เงินสด','Voucher'],
+            status:['Ready to send email','Email not ready to send','All emails',],
+
+            selecte: [],
             headers: [{
                     text: "Hospital Numbers",
                     align: "start",
