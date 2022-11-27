99d8ec7bae9c35f54a880b7e1d28b5491d99d295
KITTINARIN NAJAI <6431503004@lamduan.mfu.ac.th>
2022-11-27 18:02:03 +0700

    Add Pages and Images Success, False

---
+++ pages/false.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <v-row class="d-flex flex-column justify-center">
+        <center>
+          <v-img src="delete.png" contain  height="500px" width="400px">
+        </v-img>
+      </center>
+        <div class="text-center text-h5 font-weight-bold grey--text">
+          Email sent successfully !!
+        </div>
+      </v-row>
+      <v-row class="d-flex justify-end">
+        <v-btn class="white--text" color="#00ADB5" to="/">Back</v-btn>
+      </v-row>
+    </v-container>
+  </template>
