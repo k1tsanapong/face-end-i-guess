8caae231fad1ae3fa072622294cf0ce7797480de
KITTINARIN NAJAI <6431503004@lamduan.mfu.ac.th>
2022-11-30 18:51:58 +0700

    add version page

---
+++ pages/version.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row class="d-flex flex-column justify-center">
+      <center>
+        <v-img src="favicon2.ico" contain height="400px" width="400px"> </v-img>
+      </center>
+      <div class="text-center text-h5 font-weight-bold grey--text">
+        Version 1.0.0<br />
+        Email send system automatic<br />
+        medical checkup results
+      </div>
+    </v-row>
+    <v-row class="d-flex justify-end">
+      <v-btn class="white--text" color="#00ADB5" to="/">Back</v-btn>
+    </v-row>
+  </v-container>
+</template>
