51032af18dc38a49d841a89ae2d00825d1ec2b26
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2022-12-06 23:12:35 +0700

    build

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,5 +82,15 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
+    extend (config, { isDev, isClient }) {
+
+      config.node= {
+         fs: 'empty'
+       }
+
+      // ....
+   }
+
   }
 }
